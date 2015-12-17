89dd38cac62522dfe66d14470b8dd693fa18e64c
bnicholes <bnicholes@137.65.221.73>
2015-12-17 10:30:56 -0700

    Add application insights hooks

--- src/main/java/com/novell/testgitapp/App.java
+++ src/main/java/com/novell/testgitapp/App.java
@@ -1,16 +1,25 @@
 package com.novell.testgitapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        // Adding a comment line
-        // this is it
-        // Third comment
+import com.microsoft.applicationinsights.TelemetryClient;
+
+public class App {
+
+    private static TelemetryClient telemetry = new TelemetryClient();
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        telemetry.getContext().setInstrumentationKey("4ddd203b-b96e-43e1-b781-c5efc6224be5");
+        
+//        EventTelemetry eventTelemetry = new EventTelemetry();
+//        eventTelemetry.
+        for (int x=0; x < 50; x++) {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {}
+            telemetry.trackEvent("byEvent");
+        }
+        
+        telemetry.
     }
 }
